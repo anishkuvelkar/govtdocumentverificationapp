29c4dba3e7d63fa8df2a7d9f67b984537f2550e0
anishkuvelkar <anishkuvelkar88@gmail.com>
2025-08-10 22:04:11 -0700

    Add CORS debug endpoints last please

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,20 +1,18 @@
 // routes/authRoutes.js
 const express = require('express');
 const router = express.Router();
-const cors = require('cors');
+// const cors = require('cors'); // Remove this line
 const {test,registerUser,logoutUser,loginUser,getProfile,submitRequest, getMyRequests,uploadToUploadcare,getAdmin1Requests, approveRequest, rejectRequest,getAdmin2Requests,approveByAdmin2,rejectByAdmin2} = require('../controllers/authControllers')
 const multer = require("multer");
 const upload = multer({ storage: multer.memoryStorage() });
 
-
-
-// middleware
-router.use(
-  cors({
-    credentials: true,
-    origin: 'http://localhost:5173'
-  })
-);
+// REMOVE THIS ENTIRE SECTION:
+// router.use(
+//   cors({
+//     credentials: true,
+//     origin: 'http://localhost:5173'
+//   })
+// );
 
 router.get('/', test);
 router.post('/register', registerUser);
@@ -30,4 +28,5 @@ router.get("/admin2/requests", getAdmin2Requests);
 router.post("/admin2/request/:id/approve", approveByAdmin2);
 router.post("/admin2/request/:id/reject", rejectByAdmin2);
 router.post("/logout", logoutUser);
+
 module.exports = router;
